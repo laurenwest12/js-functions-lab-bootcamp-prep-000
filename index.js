e5e0e744b23eb01a1b258c7dc3d89b81744f5954
Lauren West <laurenwest12@gmail.com>
2018-05-26 18:59:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ return `Happy holidays, ${name}!`
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`
+}
